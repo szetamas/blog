c53765f3058527827f9a02c98111c42966465ad2
Tamas Szedlak <tamas94@protonmail.com>
2018-09-11 17:22:49 +0200

    Update load.js

--- load.js
+++ load.js
@@ -12,7 +12,7 @@ function load(cikdb, zarostr, where, atr, val)
 	xmlhttpreq.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200)		//if the ready is 4(req is done and res is ready) AND status is 200 
 	{
-		document.getElementById(where).innerHTML=this.responseText;		//text into an out tag
+		document.getElementById(where).innerHTML+=this.responseText;		//text into an out tag
 	}
 	};
 	xmlhttpreq.open("GET","/post/allpost.php?db="+cikdb+"&zar="+zarostr+"&atr="+atr+"&val="+val, true);		//send req to the php(response will be some post's some string)
